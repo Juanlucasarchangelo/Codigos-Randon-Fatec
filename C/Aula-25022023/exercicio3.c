e8214aebb1a512a80f9eb0c8c84b100d9cbdd6f3
Juanlucasarchangelo <jlarchangelo@outlook.com>
2023-02-25 12:25:03 -0300

    aula do dia 25 de fevereiro

--- C/Aula-25022023/exercicio3.c
+++ C/Aula-25022023/exercicio3.c
@@ -3,8 +3,8 @@
 
 int main()
 {
-   double ordenado = 1200;
+   double ordenado = 1200, aumento;
 
-   ordenado > 1000 ? ordenado = ordenado + (ordenado * 0.05) : ordenado = ordenado + (ordenado * 0.07);
+   aumento = ordenado > 1000 ? ordenado = ordenado + (ordenado * 0.05) : ordenado = ordenado + (ordenado * 0.07);
     return 0;
 }
