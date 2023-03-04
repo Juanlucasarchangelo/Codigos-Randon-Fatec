62ad484fffcfb46358324e4dace98c6d0ca5a4ba
Juanlucasarchangelo <jlarchangelo@outlook.com>
2023-03-04 10:42:24 -0300

    Uso do while

--- C/Aula-04032023/ex1.c
+++ C/Aula-04032023/ex1.c
@@ -3,6 +3,25 @@
 
 int main()
 {
-    printf("Apenas um teste");
-    return 0;
+    
+    int x, soma = 0;
+    
+    do
+    {
+        printf("Entre com um número: \n");
+        scanf("%d", &x);
+
+        if (x > 0 && x < 20 )
+        {
+            if(soma+x > 100){
+                break;
+            }
+            printf("Soma: %d \n", soma += x);
+        } else {
+            printf("Número inválido! \n");
+        }
+
+    } while (soma <= 100);
+
+    printf("O valor chegou a %d", soma);
 }
