5974ca2c39615a51ba2b854ac35819e464378215
Juanlucasarchangelo <jlarchangelo@outlook.com>
2023-03-25 12:33:47 -0300

    Correção do arquivo main

--- C/Aula-25032023/funcao-main-calculadora.c
+++ C/Aula-25032023/funcao-main-calculadora.c
@@ -1,38 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "funcoes\calculadora.h"
-
-int main()
-{
-    int  a, b;
-
+#include <locale.h>
+#include "calculadora.c"
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    int a, b;
     int opcao;
-
     do{
         montar_menu();
         scanf("%d", &opcao);
-        a = ler_dado();
-        b = ler_dado();
-
-        switch (opcao)
-        {
+        if(opcao>=1 && opcao<=4){
+            a = ler_dado();
+            b = ler_dado();
+        }
+        switch (opcao) {
             case 1:
-                printf("Soma: %d \n", soma(a, b));
+                printf("Soma: %d \n", somar(a, b));
                 break;
             case 2:
-                printf("Soma: %d \n", subtrair(a, b));
+                printf("Subtração: %d \n", subtrair(a, b));
                 break;
             case 3:
-                printf("Soma: %d \n", multiplicar(a, b));
+                printf("Multiplicação: %d \n", multiplicar(a,b));
                 break;
             case 4:
-                printf("Soma: %d \n", dividir(a, b));
+                printf("Divisao: %d \n", dividir(a, b));
                 break;
             case 9:
+                printf("FIM \n");
                 break;
             default:
-                printf("Número inválido!\n");
-                break;
+                printf("Opção inválida \n");
+
         }
     }while(opcao!=9);
     return 0;
